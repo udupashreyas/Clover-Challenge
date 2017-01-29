080cde1f1f7d907443e9e0fec56313d5be5ae166
udupashreyas <udupa_shreyas@yahoo.co.in>
2017-01-28 21:04:10 -0800

    added user alert

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,17 @@ function generateShape() {
 	var posx = (Math.random() * (window.innerWidth - divsize)).toFixed();
     var posy = (Math.random() * (window.innerHeight - divsize)).toFixed();
     var text = '<input type="text" style="display:block; margin:auto; width:' + (divsize/4).toString() + 'px;" />';
-	document.body.innerHTML += '<div class="shape" style="position:absolute;width:' + divsize.toString() + 'px;height:' + divsize.toString() + 'px;z-index:100;background:' + color + '; border-radius:' + radiusSize.toString() + 'px; left:' + posx.toString() + 'px; top:' + posy.toString() + 'px">' + text + '</div>';
+	document.body.innerHTML += '<div class="shape" onclick="alertUser(this)" style="position:absolute;width:' + divsize.toString() + 'px;height:' + divsize.toString() + 'px;z-index:100;background:' + color + '; border-radius:' + radiusSize.toString() + 'px; left:' + posx.toString() + 'px; top:' + posy.toString() + 'px">' + text + '</div>';
 }
 function removeShapes() {
 	var shapes = document.getElementsByClassName('shape');
 	while(shapes.length > 0){
         shapes[0].parentNode.removeChild(shapes[0]);
     }
+}
+function alertUser(divObj) {
+	console.log(divObj);
+	var text = divObj.firstChild.value;
+	console.log(text);
+	alert("You have input " + text + ". The color is " + divObj.style.background);
 }
