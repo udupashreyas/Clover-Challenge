efd27e3c6996dfad9e0d0521f6fb7b04d6e14e94
udupashreyas <udupa_shreyas@yahoo.co.in>
2017-01-28 20:47:50 -0800

    added center aligned textbox

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function generateShape() {
 	var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
 	var posx = (Math.random() * (window.innerWidth - divsize)).toFixed();
     var posy = (Math.random() * (window.innerHeight - divsize)).toFixed();
-	document.body.innerHTML += '<div style="position:absolute;width:' + divsize.toString() + 'px;height:' + divsize.toString() + 'px;opacity:0.3;z-index:100;background:' + color + '; border-radius:' + radiusSize.toString() + 'px; left:' + posx.toString() + 'px; top:' + posy.toString() + 'px"></div>';
+    var text = '<input type="text" style="display:block; margin:auto; width:' + (divsize/4).toString() + 'px;" />';
+	document.body.innerHTML += '<div style="position:absolute;width:' + divsize.toString() + 'px;height:' + divsize.toString() + 'px;z-index:100;background:' + color + '; border-radius:' + radiusSize.toString() + 'px; left:' + posx.toString() + 'px; top:' + posy.toString() + 'px">' + text + '</div>';
 	//document.getElementsByClassName('shape')[0].style.borderRadius= num+'px';
 }
